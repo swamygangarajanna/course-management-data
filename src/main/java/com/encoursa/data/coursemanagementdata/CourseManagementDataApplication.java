a3c28ff73910ae5851f4e29c535be4e64f6825c0
swamymg <swamy.m.gangaraj@gmail.com>
2019-12-21 08:09:00 +1100

    Initial commit for course management

---
+++ src/main/java/com/encoursa/data/coursemanagementdata/CourseManagementDataApplication.java
@@ -0,0 +1,13 @@
+package com.encoursa.data.coursemanagementdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementDataApplication.class, args);
+	}
+
+}
